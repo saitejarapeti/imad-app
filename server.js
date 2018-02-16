aa6fe6eb073960328ada9481dbc490c1847504f8
saitejarapeti <saiteja.rapeti@gmail.com>
2018-02-16 21:43:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/activity_one',function(req,res){
-    res.sendfile('this is the first activity')
+    res.send('this is the first activity')
 });
 app.get('/activity_two',function(req,res){
     res.send('this is the second activity')
@@ -18,7 +18,7 @@ app.get('/activity_three',function(req,res){
     res.send('this is the third activity')
 });
 app.get('/ui/style.css', function (req, res) {
-  res.send(path.join(__dirname, 'ui', 'style.css'));
+  res.sendfile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
