b747d250bfc1af6f70d35513f1fe549b25bc8014
saitejarapeti <saiteja.rapeti@gmail.com>
2018-02-18 22:53:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,22 @@
 console.log('Loaded!');
 var img =document.getElementById('madi');
 var margineleft=0;
+var marginright=0;
 function moveRight(){
     margineleft+=10;
     img.style.marginLeft=margineleft+'px';
+    if(margineleft>100){
+      var interval = setInterval(moveLeft,100)
+    margineleft=0;
+        }
+}
+function moveLeft(){
+    marginright+=10;
+    img.style.marginRight=marginright+'px';
+    if(marginright>100){
+        var interval=setInterval(moveRight,100);
+        marginright=0;
+    }
 }
 img.onclick=function(){
     var interval=setInterval(moveRight,100);
