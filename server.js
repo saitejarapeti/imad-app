d448c72c0e18569be43ecbe165a1ee903bafe0ac
saitejarapeti <saiteja.rapeti@gmail.com>
2018-02-18 22:43:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.get('/activity_two',function(req,res){
 app.get('/activity_three',function(req,res){
  res.sendFile(path.join(__dirname, 'ui', 'artical-three.html'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendfile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendfile(path.join(__dirname, 'ui', 'style.css'));
 });
